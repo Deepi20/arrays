95e783a2bcba9d78c8520705c2a7c734bf2229ea
Deepika <32121254+Deepi20@users.noreply.github.com>
2019-06-09 23:58:00 -0500

    Create arrayman.py

---
+++ arrayman.py
@@ -0,0 +1,24 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the arrayManipulation function below.
+
+n, m = map(int, raw_input().split())
+v = [0] * (n + 1)
+
+for _ in range(m):
+    a, b, k = map(int, raw_input().split())
+    v[a] += k
+    if b + 1 <= n:
+        v[b + 1] -= k
+
+max_sum = 0
+s = 0
+for i in v:
+    s += i
+    max_sum = max(max_sum, s)
+
+print(max_sum)
