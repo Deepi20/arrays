e9412a560fbe878689ab2649b4d09a17a51e8839
Deepika <32121254+Deepi20@users.noreply.github.com>
2019-06-10 23:58:48 -0500

    Create leftrotate.py

---
+++ leftrotate.py
@@ -0,0 +1,31 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the rotLeft function below.
+def rotLeft(a, d):
+    for i in range(d):
+        f = a[0]
+        a.remove(f)
+        a.append(f)
+    return a
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nd = raw_input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = map(int, raw_input().rstrip().split())
+
+    result = rotLeft(a, d)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
