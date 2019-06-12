ca5c35e334b10ee111dad6ab10be19b45d49882f
Deepika <32121254+Deepi20@users.noreply.github.com>
2019-06-11 22:26:39 -0500

    Create maxhourglasssum.py

---
+++ maxhourglasssum.py
@@ -0,0 +1,33 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the hourglassSum function below.
+def hourglassSum(arr):
+    maxVal=-9999
+    for i in range(0,4):
+        for j in range(0,4):
+            firstRow = arr[i][j]+arr[i][j+1]+arr[i][j+2]
+            thirdRow=  arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2]
+            secondRow= arr[i+1][j+1]
+            total=firstRow+secondRow+thirdRow
+            maxVal=max(maxVal,total)
+    return (maxVal)
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in xrange(6):
+        arr.append(map(int, raw_input().rstrip().split()))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
